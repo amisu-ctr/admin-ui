18caff2fbfa6f8b04b2d5556ca0476fcbe849f56
amisu <hamisurahman@gmail.com>
2023-12-09 10:15:54 +0000

    Routing and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import Home from './pages/home/Home'
+import Users from './pages/users/Users'
+import Products from './pages/products/Products'
+
+import {createBrowserRouter,RouterProvider,Route,Link} from 'react-router-dom'
+
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <Home />
+      )
+    },
+    {
+      path: 'users',
+      element: <Users />
+    },
+    {
+      path: 'products',
+      element: <Products />
+    },
+  ])
+
+
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
 
